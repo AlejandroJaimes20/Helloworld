be509f29e3b239a5ac612eb9fc82e8cf82849661
AlejandroJaimes20 <wormparade2024@gmail.com>
2025-04-27 04:32:07 -0700

    Se agrega el proyecto

--- HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61d9bc40e6d801463106eead053edc88a5fa6e2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
